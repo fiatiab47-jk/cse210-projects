7dcca674c2d0574c40531ba6b9006bfabd3d5c2b
fiatiab47-jk <afiati@byupathway.edu>
2026-01-06 13:46:43 -0800

    Added input and output

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!");
+        Console.WriteLine("This is in C#.");
+
+        Console.WriteLine("What is your favorite music? ");
+        string music = Console.ReadLine();
+        Console.WriteLine($"Your favorite music is {music}");
     }
 }
