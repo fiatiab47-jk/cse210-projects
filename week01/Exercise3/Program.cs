b848ecaf061464868c4b81c4753202abea54fe5b
fiatiab47-jk <afiati@byupathway.edu>
2026-01-10 21:52:20 -0800

    Completed Exercise 3, 4, and 5

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        // Generate Random number
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); 
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        int guessCount = 1;
+
+        while (guess != magicNumber)
+        {
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount++;
+        }
+
+        Console.WriteLine($"You guessed it in {guessCount} guesses!");
+    }    
 }
