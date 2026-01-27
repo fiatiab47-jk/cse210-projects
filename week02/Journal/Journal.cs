8e7144180421eb84ea99386850b2eef4a63c5d88
fiatiab47-jk <afiati@byupathway.edu>
2026-01-27 08:15:11 -0800

    Add comments to Journal.cs

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -22,32 +22,39 @@ public class Journal
         }
     }
 
+    // Saves entries to a file
     public void SaveToFile(string filename)
     {
+        // Opens file for writing
         using (StreamWriter writer = new StreamWriter(filename))
         {
             foreach (Entry entry in _entries)
             {
+                // Stores entry data in one line (~) is used as a separator
                 writer.WriteLine($"{entry._date}~{entry._promptText}~{entry._entryText}");
             }
         }
     }
 
+    // Loads journal data form a file
     public void LoadFromFile(string filename)
     {
+        // Prevents errors if file is missing
         if (!File.Exists(filename))
         {
             Console.WriteLine("File not Found!");
             return;
         }
-
+        // Removes old data before loading new ones
         _entries.Clear();
         string[] lines = File.ReadAllLines(filename);
 
         foreach (string line in lines)
         {
+            // Splits stored text back into fields
             string[] parts = line.Split('~');
             
+            // Creates new entry
             Entry entry = new Entry();
             entry._date = parts[0];
             entry._promptText = parts[1];
